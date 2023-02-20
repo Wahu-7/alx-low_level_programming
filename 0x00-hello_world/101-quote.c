672fd4f5d1c40fbf3ce4a22df2514a44c4846b20
Wahu-7 <wahumutuac@gmail.com>
2023-02-20 05:44:27 -0800

    printing a line

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program to print a line in the standard error
+ * Return 1 (Succeess)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	return (1);
+}

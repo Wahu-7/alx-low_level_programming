daf6131ced4bf763daec73efd46365ace85f9194
Wahu-7 <wahumutuac@gmail.com>
2023-02-20 10:16:14 -0800

    the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This program Prints the alphabet in lower case.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+

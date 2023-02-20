a0e0a26ebd18ee2bb8b22fd640d7517e17427d9e
Wahu-7 <wahumutuac@gmail.com>
2023-02-20 02:42:48 -0800

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - use puts function to print a line of code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

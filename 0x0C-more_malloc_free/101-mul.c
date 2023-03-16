f58a1f2a82eb4658c19a82bbc68760707ae12209
Wahu-7 <wahumutuac@gmail.com>
2023-03-16 16:51:54 -0700

    mltiplication

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,102 +1,39 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#define ERR_MSG "Error"
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-int is_digit(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
+ * main - resulttiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
+	unsigned long result;
+	int i, j;
 
-	int a, b, len, i, c, num1, num2, *result, a = 0;
-
-	s1 = argv[1], s2 = argv[2];
-
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	a = _strlen(s1);
-	b = _strlen(s2);
-	len = a + b + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= a + b; i++)
-		result[i] = 0;
-	for (a = a - 1; a >= 0; a--)
+	if (argc != 3)
 	{
-		num1 = s1[a] - '0';
-		c = 0;
-		for (b = _strlen(s2) - 1; b >= 0; b--)
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			num2 = s2[b] - '0';
-			c += result[a + b + 1] + (num1 * num2);
-			result[a + b + 1] = c % 10;
-			c /= 10;
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+
 		}
-		if (c > 0)
-			result[a + b + 1] += c;
+
 	}
-	for (i = 0; i < len - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
+
+	result = atol(argv[1]) * atol(argv[2]);
+
+	printf("%lu\n", result);
+
 	return (0);
+
 }
